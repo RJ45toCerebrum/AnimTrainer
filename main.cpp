64fd74ccf6ced929a9fac903e4dcf8a5350b8e0a
Tyler.Heers <Tyler.heers@gmail.com>
2026-04-15 12:49:24 -0700

    Basic cube render
    Basic cube render and camera movement. Janky camera movement that is...

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
-import std;
+#include <iostream>
+#include <raylib.h>
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(1920, 1080, "AnimTrainer - C++26 Classic");
+
+    Camera camera = { 0 };
+    camera.position = Vector3{ 10.0f, 10.0f, 10.0f };
+    camera.target = Vector3{ 0.0f, 0.0f, 0.0f };
+    camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        UpdateCamera(&camera, CAMERA_FREE);
+        BeginDrawing();
+        {
+            ClearBackground(RAYWHITE);
+            {
+                BeginMode3D(camera);
+                {
+                    DrawCube(Vector3{ 0, 0, 0 }, 2.0f, 2.0f, 2.0f, RED);
+                    DrawCubeWires(Vector3{ 0, 0, 0 }, 2.0f, 2.0f, 2.0f, MAROON);
+                    DrawGrid(10, 1.0f);
+                }
+                EndMode3D();
+            }
+            if (GuiButton(Rectangle{ 10, 10, 100, 30 }, "Reset Camera")) {
+                camera.position = Vector3{ 10.0f, 10.0f, 10.0f };
+            }
+        }
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
